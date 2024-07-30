21462a223781494b42864d02a6409cab2787c267
Tyler Bennett <tbennett@Studio.local>
2024-07-30 00:21:19 -0500

    make this more extensible

--- subsnitch/__main__.py
+++ subsnitch/__main__.py
@@ -43,27 +43,31 @@ def explode(filename):
     base_name, extension = os.path.splitext(filename)
     return base_name, extension
 
-def find_vtt_file(directory, filename, extension):
+def find_x_file(directory, filename, extension, needle):
     """
-    Searches the specified directory for a .vtt file that matches the
+    Searches the specified directory for an abritrary extension that matches the
     filename or filename.ext.
 
     Raises:
-        FileNotFoundError: If no matching .vtt file is found.
+        FileNotFoundError: If no matching .needle file is found.
     """
     ## Sanity check. Prepend a dot to extension
     extension = '.' + extension.lstrip('.')
+    needle = '.' + needle.lstrip('.')
 
-    vtt_filename = f"{filename}.vtt"
-    vtt_with_ext_filename = f"{filename}{extension}.vtt"
+    needle_filename = f"{filename}{needle}"
+    needle_with_ext_filename = f"{filename}{extension}{needle}"
     files_in_directory = os.listdir(directory)
 
-    if vtt_filename in files_in_directory:
-        return os.path.join(directory, vtt_filename)
-    elif vtt_with_ext_filename in files_in_directory:
-        return os.path.join(directory, vtt_with_ext_filename)
+    if needle_filename in files_in_directory:
+        return os.path.join(directory, needle_filename)
+    elif needle_with_ext_filename in files_in_directory:
+        return os.path.join(directory, needle_with_ext_filename)
     else:
         raise FileNotFoundError(f"No matching .vtt file found for '{filename}' with extension '{extension}' in directory '{directory}'.")
+
+def find_vtt_file(directory, filename, extension):
+    return find_x_file(directory, filename, extension, ".vtt")
 
 def main():
     log = begin_logging()
@@ -73,9 +77,17 @@ def main():
     current_directory = os.getcwd()
     for filename in os.listdir(current_directory):
         if filename.endswith('.wav') or filename.endswith('.mp3'):
-            base_name, extension = os.path.splitext(filename)
-            print(f"Filename: {base_name}, Extension: {extension}")
-            targets.append(filename)
+            base_name, extension = explode(filename)
+            print(f"[*] Audio:: Filename: {base_name}, Extension: {extension}")
+            subtitles = find_vtt_file(current_directory, base_name, extension)
+            base_name, extension = explode(subtitles)
+            print(f"[*] VTT:: Filename: {base_name}, Extension: {extension}")
+
+            # Looking for a cover file
+
+
+            sys.exit(1)
+            #targets.append(filename)
 
     try:
         return 0
