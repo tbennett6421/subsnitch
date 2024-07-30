c4d65362ef2a752d0b83d39eee75f131b4427b5a
Tyler Bennett <tbennett@Studio.local>
2024-07-30 00:00:16 -0500

    refactor main

--- subsnitch/__main__.py
+++ subsnitch/__main__.py
@@ -1,58 +1,13 @@
 #from __future__ import (print_function, unicode_literals, division, absolute_import)
 __metaclass__ = type
-__package_name__ = 'subsnitch'
+__package_name__    = 'subsnitch'
+__code_desc__       = 'burning subtitles/covers on audio tracks to produce video files'
+__code_version__    = "v0.0.1"
 
-## Standard Libraries
 import os
-import sys
 import argparse
 import logging
-from pprint import pprint#, pformat
-
-## Third Party libraries
-import pkg_resources
-
-## Modules
-# try:
-#     from .classes import CustomExceptions
-# except ImportError:
-#     # If we can't import modules, probably running from VSCODE
-#     # attempt to hack in modules
-#     import importlib
-#     SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-#     sys.path.append(os.path.dirname(SCRIPT_DIR))
-#     CustomExceptions = importlib.import_module(__package_name__+'.classes.CustomExceptions')
-
-## Load up some metadata
-try:
-    __code_name__  = __package_name__
-    # spacing is deliberate
-    __code_desc__ = """ program description to be displayed by argparse \n    ex: python {name}
-        """.format(name=str(__package_name__)+'.py')
-    _distro = pkg_resources.get_distribution(__package_name__)
-    __code_version__ = _distro.version
-    __code_meta__ = vars(_distro)
-    __code_debug__ = False
-except pkg_resources.DistributionNotFound:
-    # when debugging with vscode
-    __code_debug__ = True
-    stubs = [ '__code_version__', '__code_meta__']
-    for s in stubs:
-        globals()[s] = 'Not Available'
-
-## Functions
-def __print_dunders__():
-    blacklist = ['__builtins__', '__print_dunders__']
-    for k, v in list(globals().items()):
-        if k.startswith('__'):
-            if k in blacklist:
-                pass
-            else:
-                pprint("%s => %s" % (k, v))
-
-def demo():
-    __print_dunders__()
-    sys.exit(1)
+from pprint import pprint as pp
 
 def begin_logging():
     handler = logging.StreamHandler()
@@ -65,6 +20,7 @@ def begin_logging():
     log = logging.getLogger(__package_name__)
     log.setLevel(logging.INFO)
     log.addHandler(handler)
+    return log
 
 def collect_args():
     parser = argparse.ArgumentParser(description=__code_desc__,
@@ -80,13 +36,21 @@ def handle_args():
     return args
 
 def main():
-    begin_logging()
+    log = begin_logging()
     args = handle_args()
 
+    targets = []
+    current_directory = os.getcwd()
+    for filename in os.listdir(current_directory):
+        if filename.endswith('.wav') or filename.endswith('.mp3'):
+            base_name, extension = os.path.splitext(filename)
+            print(f"Filename: {base_name}, Extension: {extension}")
+            targets.append(filename)
+
     try:
-        return
+        return 0
     except Exception as e:
-        pprint(e)
+        pp(e)
         raise e
     finally:
         pass
