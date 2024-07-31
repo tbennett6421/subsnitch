737cac4330952a83cdf635e1ff70321aa44031de
Tyler Bennett <tbennett@Studio.local>
2024-07-30 20:21:16 -0500

    add create_subtitled_video, pathjoin, refactoring/additions

--- subsnitch/__main__.py
+++ subsnitch/__main__.py
@@ -9,6 +9,10 @@ import argparse
 import logging
 from pprint import pprint as pp
 
+## Third Party libraries
+import ffmpeg
+from moviepy.editor import ImageClip, AudioFileClip
+
 class FileNotFoundError(Exception):
     """Custom exception to be raised when the file is not found."""
     pass
@@ -39,6 +43,9 @@ def handle_args():
     parser, args = collect_args()
     return args
 
+def pathjoin(directory, filename):
+    return os.path.join(directory, filename)
+
 def explode(filename):
     base_name, extension = os.path.splitext(filename)
     return base_name, extension
@@ -60,9 +67,9 @@ def find_x_file(directory, filename, extension, needle, raisable=True):
     files_in_directory = os.listdir(directory)
 
     if needle_filename in files_in_directory:
-        return os.path.join(directory, needle_filename)
+        return pathjoin(directory, needle_filename)
     elif needle_with_ext_filename in files_in_directory:
-        return os.path.join(directory, needle_with_ext_filename)
+        return pathjoin(directory, needle_with_ext_filename)
     else:
         if raisable:
             raise FileNotFoundError(f"No matching .vtt file found for '{filename}' with extension '{extension}' in directory '{directory}'.")
@@ -73,7 +80,6 @@ def find_vtt_file(directory, filename, extension):
     return find_x_file(directory, filename, extension, ".vtt")
 
 def find_image_file(directory, filename, extension):
-
     # Search for images matching the targeted audio file
     acceptable_extensions = ['.png', '.jpg', '.jpeg', '.webp']
     for needle in acceptable_extensions:
@@ -84,7 +90,7 @@ def find_image_file(directory, filename, extension):
     # If not found, look for a fallback file named 'cover'
     for ext in acceptable_extensions:
         cover_filename = f'cover{ext}'
-        cover_path = os.path.join(directory, cover_filename)
+        cover_path = pathjoin(directory, cover_filename)
         # Check if the cover file exists
         if os.path.isfile(cover_path):
             return cover_path
@@ -93,29 +99,48 @@ def find_image_file(directory, filename, extension):
     # Images are optional
     return None
 
+def make_selections(directory):
+    targets = []
+    for filename in os.listdir(directory):
+        if filename.endswith('.wav') or filename.endswith('.mp3'):
+            # Audio Selection
+            audiofile = pathjoin(directory, filename)
+            base_name, extension = explode(filename)
+            # Subtitle Selection
+            subtitles = find_vtt_file(directory, base_name, extension)
+            imagefile = find_image_file(directory, base_name, extension)
+            target = {
+                "audio": audiofile,
+                "vtt": subtitles,
+                "img": imagefile,
+            }
+            targets.append(target)
+    return targets
+
+def create_subtitled_video(image=None, audio=None, vtt=None, output=None):
+    # Validate arguments
+    assert None not in [audio, vtt]
+    if output == None:
+        filename, extension = explode(audio)
+        basefile = os.path.basename(filename) + ".mp4"
+        output = filename + ".mp4"
+
+    print("[*] Creating subtitled video")
+    print("[*] Audio: %s" % audio)
+    print("[*]   Img: %s" % image)
+    print("[*]   VTT: %s" % vtt)
+    print("[*] Video: %s" % output)
+    print("[*] VideoFile: %s" % basefile)
+
 def main():
     log = begin_logging()
     args = handle_args()
-
-    targets = []
-    current_directory = os.getcwd()
-    for filename in os.listdir(current_directory):
-        if filename.endswith('.wav') or filename.endswith('.mp3'):
-            # Audio Selection
-            base_name, extension = explode(filename)
-            print(f"[*] Audio:: Filename: {base_name}, Extension: {extension}")
-            # Subtitle Selection
-            subtitles = find_vtt_file(current_directory, base_name, extension)
-            base_name, extension = explode(subtitles)
-            print(f"[*] VTT:: Filename: {base_name}, Extension: {extension}")
-            # Image Selection
-            imagefile = find_image_file(current_directory, base_name, extension)
-            if imagefile is not None:
-                base_name, extension = explode(imagefile)
-                print(f"[*] IMG:: Filename: {base_name}, Extension: {extension}")
-
-            sys.exit(1)
-            #targets.append(filename)
+    targets = make_selections(os.getcwd())
+    for target in targets:
+        create_subtitled_video(image=target['img'], audio=target['audio'], vtt=target['vtt'])
+        print(f"[**] Audio:{target['audio']}")
+        print(f"[**]   VTT:{target['vtt']}")
+        print(f"[**]   IMG:{target['img']}")
 
     try:
         return 0
